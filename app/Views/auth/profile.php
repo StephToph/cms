3360249f3f95c542c755f91e7d2511ab66a5010b
Adeagbo Stephen <102860831+StephToph@users.noreply.github.com>
2024-10-11 01:30:45 +0100

    update

--- app/Views/auth/profile.php
+++ app/Views/auth/profile.php
@@ -18,7 +18,7 @@
                     <div class="nk-content-wrap">
                         <div class="nk-block-head">
                             <div class="nk-block-head-content">
-                                <h3 class="nk-block-title page-title"><?=translate_phrase('My Profile'); ?></h3>
+                                <h3 class="nk-block-title page-title"><?=translate_phrase(' Profile'); ?></h3>
                                 <div class="nk-block-des">
                                     <p><?=translate_phrase('You have full control to manage your own account setting.');?></p>
                                     
@@ -27,67 +27,143 @@
                         </div><!-- .nk-block-head -->
                         <div class="nk-block">
                             <div class="card card-bordered">
-                                <ul class="nav nav-tabs nav-tabs-mb-icon nav-tabs-card">
-                                    <li class="nav-item">
-                                        <a class="nav-link active" href="<?=site_url('auth/profile'); ?>"><em class="icon ni ni-user-fill-c"></em><span><?=translate_phrase('Personal'); ?></span></a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="<?=site_url('accounts/membership/partnership/'.$log_id); ?>"><em class="icon ni ni-cc-secure"></em><span><?=translate_phrase('Partnership Goals'); ?></span></a>
-                                    </li>
-                                </ul><!-- .nav-tabs -->
-                                <div class="card-inner card-inner-lg">
-                                    <div class="nk-block-head">
-                                        <div class="nk-block-head-content">
-                                            <h4 class="nk-block-title"><?=translate_phrase('Personal Information'); ?></h4>
-                                            <div class="nk-block-des">
-                                            </div>
-                                        </div>
-                                    </div><!-- .nk-block-head -->
+                                <div class="card-inner card-inner-lg" id="profile_resp">
+                                    
                                     <div class="nk-block">
                                         <div class="nk-data data-list data-list-s2">
                                             <div class="data-head">
                                                 <h6 class="overline-title"><?=translate_phrase('Basics'); ?></h6>
                                             </div>
-                                            <div class="data-item pop" pageTitle="<?=translate_phrase('Manage Profile'); ?>" pageSize="modal-md" pageName="<?=site_url('auth/profile/manage/personal'); ?>">
-                                                <div class="data-col">
-                                                    <span class="data-label"><?=translate_phrase('Full Name');?></span>
-                                                    <span class="data-value"><?=$fullname;?></span>
-                                                </div>
-                                                <div class="data-col data-col-end"><span class="data-more"><em class="icon ni ni-forward-ios"></em></span></div>
-                                            </div><!-- data-item -->
-                                            <div class="data-item pop" pageTitle="<?=translate_phrase('Manage Profile'); ?>" pageSize="modal-md" pageName="<?=site_url('auth/profile/manage/personal'); ?>">
-                                                <div class="data-col">
-                                                    <span class="data-label"><?php 
-                                                        echo translate_phrase('Kingchat Handle');?></span>
-                                                    <span class="data-value"><?=$chat_handle; ?></span>
-                                                </div>
-                                                <div class="data-col data-col-end"><span class="data-more"><em class="icon ni ni-forward-ios"></em></span></div>
-                                            </div><!-- data-item -->
+                                            
                                             <div class="data-item">
                                                 <div class="data-col">
-                                                    <span class="data-label"><?=translate_phrase('Email'); ?></span>
-                                                    <span class="data-value"><?=$email;?></span>
+                                                    <span class="data-label">Ministry</span>
+                                                    <span class="data-value"><?=ucwords($this->Crud->read_field('id', $ministry_id, 'ministry', 'name')); ?></span>
                                                 </div>
-                                                <div class="data-col data-col-end"><span class="data-more"><em class="icon ni ni-forward-ios"></em></span></div>
-                                            </div><!-- data-item -->
-                                            <div class="data-item">
-                                                <div class="data-col">
-                                                    <span class="data-label"><?=translate_phrase('Phone Number'); ?></span>
-                                                    <span class="data-value text-soft"><?=$phone;?></span>
+                                                <div class="data-col data-col-end">
+                                                    <span class="data-more disable">
+                                                        <em class="icon ni ni-lock-alt"></em>
+                                                    </span>
                                                 </div>
-                                                <div class="data-col data-col-end"><span class="data-more"><em class="icon ni ni-forward-ios"></em></span></div>
-                                            </div><!-- data-item -->
-                                            <div class="data-item pop" pageTitle="<?=translate_phrase('Manage Profile');?>" pageSize="modal-md" pageName="<?=site_url('auth/profile/manage/personal'); ?>">
+                                            </div>
+                                            <div class="data-item pop" pageTitle="<?=translate_phrase('Manage Profile');?>" pageSize="modal-md" pageName="<?=site_url('auth/profile/manage/church'); ?>">
                                                 <div class="data-col">
-                                                    <span class="data-label"><?=translate_phrase('Address'); ?></span>
-                                                    <span class="data-value"><?=$address; ?></span>
+                                                    <span class="data-label">Church</span>
+                                                    <span class="data-value"><?=ucwords($church_name); ?></span>
                                                 </div>
                                                 <div class="data-col data-col-end"><span class="data-more"><em class="icon ni ni-forward-ios"></em></span></div>
                                             </div>
-                                           
+                                            <div class="data-item pop" pageTitle="<?=translate_phrase('Manage Profile');?>" pageSize="modal-md" pageName="<?=site_url('auth/profile/manage/church'); ?>">
+                                                <div class="data-col">
+                                                    <span class="data-label">Church Email</span>
+                                                    <span class="data-value"><?=($church_email); ?></span>
+                                                </div>
+                                                <div class="data-col data-col-end"><span class="data-more"><em class="icon ni ni-forward-ios"></em></span></div>
+                                            </div>
+                                            <div class="data-item pop" pageTitle="<?=translate_phrase('Manage Profile');?>" pageSize="modal-md" pageName="<?=site_url('auth/profile/manage/church'); ?>">
+                                                <div class="data-col">
+                                                    <span class="data-label">Church Phone</span>
+                                                    <span class="data-value"><?=ucwords($church_phone); ?></span>
+                                                </div>
+                                                <div class="data-col data-col-end"><span class="data-more"><em class="icon ni ni-forward-ios"></em></span></div>
+                                            </div>
+                                            <div class="data-item pop" pageTitle="<?=translate_phrase('Manage Profile');?>" pageSize="modal-md" pageName="<?=site_url('auth/profile/manage/church'); ?>">
+                                                <div class="data-col">
+                                                    <span class="data-label">Church Address</span>
+                                                    <span class="data-value"><?=ucwords($church_address); ?></span>
+                                                </div>
+                                                <div class="data-col data-col-end"><span class="data-more"><em class="icon ni ni-forward-ios"></em></span></div>
+                                            </div>
+                                            <div class="data-item pop" pageTitle="<?=translate_phrase('Manage Profile');?>" pageSize="modal-md" pageName="<?=site_url('auth/profile/manage/church'); ?>">
+                                                <div class="data-col">
+                                                    <span class="data-label">Church Country</span>
+                                                    <span class="data-value">
+                                                        <?php
+                                                            if(empty($church_country)){
+                                                                echo 'Country Not Set';
+                                                                $currency = '';
+                                                            } else {
+                                                                echo $this->Crud->read_field('id',  $church_country, 'country', 'name').' ('.ucwords($this->Crud->read_field('id',  $church_country, 'country', 'currency_name')).')';
+                                                            }
+                                                        ?>
+                                                    </span>
+                                                </div>
+                                                <div class="data-col data-col-end"><span class="data-more"><em class="icon ni ni-forward-ios"></em></span></div>
+                                            </div>
+                                            <div class="data-item">
+                                                <div class="data-col">
+                                                    <span class="data-label">Default Currency</span>
+                                                    <span class="data-value">
+                                                        <?php
+                                                            if(empty($church_currency)){
+                                                                echo 'Espees - ESP';
+                                                            }else{
+                                                                echo $this->Crud->read_field2('country_id',  $church_country, 'ministry_id', $ministry_id, 'currency', 'currency_name').' - '. $this->Crud->read_field2('country_id',  $church_country, 'ministry_id', $ministry_id, 'currency', 'symbol');
+                                                            }
+                                                        ?>
+                                                    </span>
+                                                </div>
+                                                <div class="data-col data-col-end">
+                                                    <span class="data-more disable">
+                                                        <em class="icon ni ni-lock-alt"></em>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <?php if($church_region){?>
+                                            <div class="data-item">
+                                                <div class="data-col">
+                                                    <span class="data-label">Regional Church</span>
+                                                    <span class="data-value"><?=ucwords($this->Crud->read_field('id', $church_region, 'church', 'name')); ?></span>
+                                                </div>
+                                                <div class="data-col data-col-end">
+                                                    <span class="data-more disable">
+                                                        <em class="icon ni ni-lock-alt"></em>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <?php } ?>
+                                            <?php if($church_zone){?>
+                                            <div class="data-item">
+                                                <div class="data-col">
+                                                    <span class="data-label">Zonal Church</span>
+                                                    <span class="data-value"><?=ucwords($this->Crud->read_field('id', $church_zone, 'church', 'name')); ?></span>
+                                                </div>
+                                                <div class="data-col data-col-end">
+                                                    <span class="data-more disable">
+                                                        <em class="icon ni ni-lock-alt"></em>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <?php }?>
+                                            <?php if($church_group){?>
+                                            <div class="data-item">
+                                                <div class="data-col">
+                                                    <span class="data-label">Group Church</span>
+                                                    <span class="data-value"><?=ucwords($this->Crud->read_field('id', $church_group, 'church', 'name')); ?></span>
+                                                </div>
+                                                <div class="data-col data-col-end">
+                                                    <span class="data-more disable">
+                                                        <em class="icon ni ni-lock-alt"></em>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <?php } ?>
+                                            <!-- <div class="data-item">
+                                                <div class="data-col">
+                                                    <span class="data-label">Pastor-in-Charge</span>
+                                                    <span class="data-value">
+                                                        <?=ucwords($this->Crud->read_field('id', $church_pastor, 'user', 'title').' '.$this->Crud->read_field('id', $church_pastor, 'user', 'firstname').' '.$this->Crud->read_field('id', $church_pastor, 'user', 'surnmae')); ?>
+                                                    </span>
+                                                </div>
+                                                <div class="data-col data-col-end">
+                                                    <span class="data-more disable">
+                                                        <em class="icon ni ni-lock-alt"></em>
+                                                    </span>
+                                                </div>
+                                            </div> -->
                                         </div><!-- data-list -->
                                     </div><!-- .nk-block -->
-                                </div><!-- .card-inner -->
+                                </div>
                             </div><!-- .card -->
                         </div><!-- .nk-block -->
                     </div>
