41166325d67a509a22c661ff7d6d384c31a44a48
Adeagbo Stephen <102860831+StephToph@users.noreply.github.com>
2024-10-22 13:37:02 +0100

    hg

--- app/Views/activity/list.php
+++ app/Views/activity/list.php
@@ -10,7 +10,7 @@
 
 <?=$this->section('content');?>
 <div class="nk-content">
-        <div class="container wide-xl ">
+        <div class="container">
             <div class="nk-content-inner mt-5">
             <div class="nk-content-body">
                 <div class="nk-block-head nk-block-head-sm  mt-5">
